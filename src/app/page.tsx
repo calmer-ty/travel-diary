ee858d4daaadd55620670c8aa8809ab65fff5db4
Your Name <you@example.com>
2025-06-02 17:35:04 +0900

    코드 정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { useAuth } from "@/commons/hooks/useAuth";
 import IntroPage from "./intro";
 
 import Maps from "@/component/maps";
-import MainPage from "@/component/mainpage";
+// import MainPage from "@/component/mainpage";
 
 export default function Home() {
-  return <MainPage />;
-  // const { user } = useAuth();
-  // return <>{user === null ? <IntroPage /> : <Maps />}</>;
+  // return <MainPage />;
+  const { user } = useAuth();
+  return <>{user === null ? <IntroPage /> : <Maps />}</>;
 }
