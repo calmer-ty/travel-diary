ee41e1b4b6303f60532ee45b7a607b490e4472fd
Your Name <you@example.com>
2025-06-02 17:33:54 +0900

    style 메인 화면 스타일 수정

--- src/component/mainpage/index.tsx
+++ src/component/mainpage/index.tsx
@@ -3,9 +3,9 @@ export default function MainPage() {
     <div className="relative w-screen h-screen overflow-hidden">
       <div className="relative size-full p-10">
         <h2 className=" text-2xl font-medium pt-10">나의 여행 일기</h2>
-        {/* bg-[url(/images/img_diary.png)] bg-contain bg-no-repeat */}
-        <div className="relative size-full  mt-10">
-          <div className=" flex flex-wrap justify-center items-center gap-18 w-full ">
+
+        <div className="relative w-full  mt-10  rounded-3xl bg-[#fff] p-10 pt-20 pb-20 shadow-[12px_12px_0px_#ddd] border border-solid border-gray-300">
+          <div className="flex flex-wrap justify-center items-center gap-18 w-full ">
             <div className="text-center text-lg justify-center items-center break-keep flex flex-col gap-5 ">
               <button className="w-30 h-30 bg-[#F5F4A9]  shadow-[3px_3px_0px_#CDCC86] rounded-[100%] hover:shadow-[-3px_-3px_0px_#CDCC86]">
                 <img src="/images/icon_diary.png" alt="" className="ml-auto mr-auto size-ful object-contain" />
@@ -31,6 +31,8 @@ export default function MainPage() {
               <span>여행 구경하기</span>
             </div>
           </div>
+
+          <div className="absolute top-[50%] left-[-20] h-full  w-9.5 bg-[url(/images/icon_spring.png)] bg-contain bg-no-repeat bg-center z-10 transform-[translateY(-50%)]"></div>
         </div>
       </div>
     </div>
