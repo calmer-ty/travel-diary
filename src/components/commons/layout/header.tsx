0639ba246aaeb9786450c5e7638b271d1e1f4840
Calmer <ty.calmer96@gmail.com>
2025-06-05 12:23:05 +0900

    feat: 가짜 헤더 추가 및 main 태그 추가 레이아웃 스타일 수정

--- src/components/commons/layout/header.tsx
+++ src/components/commons/layout/header.tsx
@@ -2,11 +2,8 @@
 
 import { signInWithPopup } from "firebase/auth";
 import { auth, googleProvider } from "@/commons/libraries/firebase/firebaseApp";
-import { useRouter } from "next/router";
 
 export default function Header() {
-  //const router = useRouter();
-
   // Google 로그인 처리
   const handleGoogleLogin = async (): Promise<void> => {
     try {
@@ -18,22 +15,25 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white w-full h-[50px] p-1 shadow-[0_3px_3px_rgba(0,0,0,0.2)] fixed top-[0] z-[10]">
-      <div className="w-full h-full flex justify-between items-center">
-        <h1 className="text-[0px] w-30 h-8 bg-[url(/images/Logo.png)] bg-contain bg-no-repeat">Travel Diary</h1>
+    <>
+      <header className="w-full h-10 p-1 bg-white shadow-[0_3px_3px_rgba(0,0,0,0.2)] fixed top-0 z-10">
+        <div className="w-full h-full flex justify-between items-center">
+          <h1 className="text-[0px] w-30 h-8 bg-[url(/images/Logo.png)] bg-contain bg-no-repeat">Travel Diary</h1>
 
-        {auth.currentUser?.uid ? (
-          <div className="flex items-center gap-2 font-medium">
-            <img className="w-4 h-4" src="/images/icon_people.png" alt="" />
-            <span>{auth.currentUser.displayName}</span>
-          </div>
-        ) : (
-          // <button onClick={handleGoogleLogout} className="font-medium ">로그 아웃</button>
-          <button onClick={handleGoogleLogin} className="font-medium ">
-            로그인
-          </button>
-        )}
-      </div>
-    </header>
+          {auth.currentUser?.uid ? (
+            <div className="flex items-center gap-2 font-medium">
+              <img className="w-4 h-4" src="/images/icon_people.png" alt="" />
+              <span>{auth.currentUser.displayName}</span>
+            </div>
+          ) : (
+            // <button onClick={handleGoogleLogout} className="font-medium ">로그 아웃</button>
+            <button onClick={handleGoogleLogin} className="font-medium ">
+              로그인
+            </button>
+          )}
+        </div>
+      </header>
+      <div className="fake-header w-full h-10"></div>
+    </>
   );
 }
