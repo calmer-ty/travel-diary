2c83439b4f0dd2ea909452fe4d11d4914cba6ad2
Calmer <ty.calmer96@gmail.com>
2025-06-08 18:31:35 +0900

    feat: 데이터 피커 컴포넌트 생성

---
+++ src/components/commons/datePicker/01.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import { Calendar as CalendarIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+
+export function DatePicker01() {
+  const [open, setOpen] = React.useState(false);
+  const [date, setDate] = React.useState<Date | undefined>(undefined);
+
+  return (
+    <div className="flex flex-col gap-3">
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button variant="outline" id="date" className="!flex">
+            <CalendarIcon size={24} />
+            <span>{date ? date.toLocaleDateString() : "기록할 날짜를 선택하세요."}</span>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto overflow-hidden p-0" align="start">
+          <Calendar
+            mode="single"
+            selected={date}
+            captionLayout="dropdown"
+            onSelect={(date) => {
+              setDate(date);
+              setOpen(false);
+            }}
+          />
+        </PopoverContent>
+      </Popover>
+    </div>
+  );
+}
