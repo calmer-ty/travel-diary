fe023979dcdfaa1926f5b5f9647c6b2024cf755c
Calmer <ty.calmer96@gmail.com>
2025-07-08 17:18:35 +0900

    feat: 메인페이지 list 링크 요소 추가(임시)

--- src/components/unit/mainPage/index.tsx
+++ src/components/unit/mainPage/index.tsx
@@ -2,6 +2,21 @@
 import Link from "next/link";
 import { motion } from "framer-motion";
 
+const mainItem = [
+  {
+    title: "/list",
+    href: "/list",
+  },
+  {
+    title: "",
+    href: "",
+  },
+  {
+    title: "",
+    href: "",
+  },
+];
+
 export default function MainPage() {
   return (
     <div className="flex-col relative size-full flex items-center p-14 sm:flex-row gap-8">
@@ -25,7 +40,7 @@ export default function MainPage() {
         </Link>
       </motion.div>
       <div className="w-full h-full flex justify-between items-center flex-col gap-8 size-full sm:w-[42%]">
-        {[0, 1, 2].map((i) => (
+        {mainItem.map((item, i) => (
           <motion.div
             key={i}
             initial={{ opacity: 0, y: 30 }}
@@ -33,7 +48,11 @@ export default function MainPage() {
             transition={{ duration: 0.2, delay: i * 0.1, ease: "easeOut" }}
             whileHover={{ scale: 1.03, boxShadow: "8px 8px 0px #888888" }}
             className="w-full h-[33.3%] bg-[#FAFAF2] shadow-[6px_6px_0px_#AAAAAA] rounded-3xl border border-gray-300 cursor-pointer"
-          />
+          >
+            <Link href={item.href} className="block size-full">
+              {item.title}
+            </Link>
+          </motion.div>
         ))}
       </div>
     </div>
