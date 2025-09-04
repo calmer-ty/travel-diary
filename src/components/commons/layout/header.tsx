652e3781e6a5975047b43162ec039a1a34111e7b
Calmer <ty.calmer96@gmail.com>
2025-09-04 18:10:02 +0900

    feat: 홈일 때는 네비 가리기

--- src/components/commons/layout/header.tsx
+++ src/components/commons/layout/header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import { useAuth } from "@/hooks/useAuth";
 
@@ -8,45 +9,48 @@ import { Button } from "@/components/ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 export default function Header() {
+  const pathname = usePathname();
   const { user, handleLogin, handleLogout } = useAuth();
 
   return (
     <>
-      {user && (
-        <header className="flex justify-between items-center w-full h-12 px-4 bg-white shadow-md fixed z-10">
-          <h1 className="w-30 h-8 bg-[url(/images/Logo.png)] bg-contain bg-no-repeat text-[0px]">
-            <Link href="/dashboard" className="block size-full">
-              Travel Diary
-            </Link>
-          </h1>
-          {user !== null ? (
-            <>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button variant="outline">
-                    <div className="flex items-center gap-2">
-                      <img className="w-4 h-4" src="/images/icon_people.png" alt="" />
-                      <span>{user.displayName}</span>
-                    </div>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-56" align="start">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem>Profile</DropdownMenuItem>
-                  </DropdownMenuGroup>
-                  <DropdownMenuItem onClick={handleLogout}>Log out</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </>
-          ) : (
-            <Button variant="outline" onClick={handleLogin}>
-              로그인
-            </Button>
-          )}
-        </header>
+      {pathname !== "/" && (
+        <>
+          <header className="flex justify-between items-center w-full h-12 px-4 bg-white shadow-md fixed z-10">
+            <h1 className="w-30 h-8 bg-[url(/images/Logo.png)] bg-contain bg-no-repeat text-[0px]">
+              <Link href="/dashboard" className="block size-full">
+                Travel Diary
+              </Link>
+            </h1>
+            {user !== null ? (
+              <>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button variant="outline">
+                      <div className="flex items-center gap-2">
+                        <img className="w-4 h-4" src="/images/icon_people.png" alt="" />
+                        <span>{user.displayName}</span>
+                      </div>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-56" align="start">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuGroup>
+                      <DropdownMenuItem>Profile</DropdownMenuItem>
+                    </DropdownMenuGroup>
+                    <DropdownMenuItem onClick={handleLogout}>Log out</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </>
+            ) : (
+              <Button variant="outline" onClick={handleLogin}>
+                로그인
+              </Button>
+            )}
+          </header>
+          <div className="h-12"></div>
+        </>
       )}
-      {user && <div className="h-12"></div>}
     </>
   );
 }
