0b90abd9849fc806df9df47677e313eb7cf93a5b
hyerin <gpfls662@gmail.com>
2025-09-06 16:53:39 +0900

    fix: 슬라이드 삭제

--- src/components/unit/started/index.tsx
+++ src/components/unit/started/index.tsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { Slider } from "./slider";
 
 export default function StartedUI() {
   const router = useRouter();
@@ -28,8 +27,8 @@ export default function StartedUI() {
   };
 
   return (
-    <section className="relative w-screen h-screen  p-10 overflow-x-hidden box-border">
-      <div className="flex gap-1">
+    <section className="relative w-screen h-screen p-8  md:p-15 overflow-x-hidden ">
+      <div className="flex gap-1 ">
         {waveText.map((el, i) => (
           <motion.span
             key={i}
@@ -40,7 +39,7 @@ export default function StartedUI() {
               delay: i * 0.1, // 글자마다 순차적으로 딜레이
               ease: "easeInOut",
             }}
-            className="inline-block  text-6xl"
+            className="inline-block  text-5xl md:text-7xl"
             style={{ color: el.color, textShadow: "0px 2px 2px rgba(0,0,0,0.35)" }}
           >
             {el.text}
@@ -48,46 +47,39 @@ export default function StartedUI() {
         ))}
       </div>
 
-      <div className="relative w-full flex flex-col items-start justify-center gap-2 mt-15 sm:flex-row">
+      <div className="flex flex-col justify-between items-start  break-keep mt-10 h-150">
         {/* text 영역 */}
-        <div className=" text-center sm:w-[47%] w-full break-keep">
-          <div className="text-4xl text-left">당신의 여행 기록을 쉽고 재미있게 기록하기!</div>
+        <div className="text-4xl ">당신의 여행 기록을 쉽고 재미있게 기록하기!</div>
 
-          <div className="text-2xl mt-5 mb-10 text-left">
-            <p>당신의 여행을 기록해 보세요!</p>
-            <p>지도에 찍고, 사진과 일기로 남기는 나만의 여행 기록.</p>
-            <p>나의 여행을 한눈에 확인하기!</p>
-            <p>재미있는 게임을 통해 친구들과 함께 내기하기!</p>
-          </div>
+        <div className="text-2xl mt-5 ">
+          <p>당신의 여행을 기록해 보세요!</p>
+          <p>지도에 찍고, 사진과 일기로 남기는 나만의 여행 기록!</p>
+          <p>나의 여행을 한눈에 확인하기!</p>
+          <p>재미있는 게임을 통해 친구들과 함께 내기하기!</p>
+        </div>
 
-          {/* img 영역*/}
-          <div className="flex items-center justify-between gap-2 w-full mb-5">
-            {moveImages.map((src, i) => (
-              <motion.img
-                key={i}
-                src={src}
-                alt=""
-                className={`w-[${i === 2 ? "26" : "27"}%]`}
-                animate={{ y: [0, -20, 0] }} // 위로 15px 떠오르고 다시 내려오는 애니메이션
-                transition={{
-                  duration: 2 + i * 0.3, // 이미지마다 약간 다른 속도
-                  repeat: Infinity, // 무한 반복
-                  ease: "easeInOut",
-                }}
-              />
-            ))}
-          </div>
+        {/* img 영역*/}
+        <div className="flex items-center justify-center md:justify-end gap-5 w-full ">
+          {moveImages.map((src, i) => (
+            <motion.img
+              key={i}
+              src={src}
+              alt=""
+              className={`w-28 md:w-48`}
+              animate={{ y: [0, -20, 0] }} // 위로 15px 떠오르고 다시 내려오는 애니메이션
+              transition={{
+                duration: 2 + i * 0.3, // 이미지마다 약간 다른 속도
+                repeat: Infinity, // 무한 반복
+                ease: "easeInOut",
+              }}
+            />
+          ))}
+        </div>
 
+        <div className="mx-auto">
           <Button variant="default" className="bg-[#E9897B] shadow-[inset_-2px_-2px_0px_#F3642A] hover:shadow-[inset_2px_2px_0px_#F3642A] hover:bg-[#E9897B]" onClick={onClickMoveToHome}>
             지금 시작하기
           </Button>
-        </div>
-
-        {/* 슬라이드 영역 */}
-        <div className="flex items-center justify-center sm:w-[47%] sm:mb-[0] w-full sm:h-140 h-80 mb-10 bg-[#E3F3FE] shadow-[inset_-5px_-5px_0px_#6DC2FF] rounded-2xl">
-          <div className="w-[90%] h-[90%]">
-            <Slider />
-          </div>
         </div>
       </div>
     </section>
