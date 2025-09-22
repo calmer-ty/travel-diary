afc2735a0a5524c4a7206c11146910dd54b02f05
hyerin <gpfls662@gmail.com>
2025-09-22 16:30:42 +0900

    fix: firebase 배포 오류 수정 중

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,8 @@
 import * as functions from "firebase-functions";
 import next from "next";
+import * as admin from "firebase-admin";
 
+admin.initializeApp();
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev, conf: { distDir: ".next" } });
 const handle = app.getRequestHandler();
