a0601501fe1e76f0cfc464cf6a73d8bd1cbf2ef1
Calmer <ty.calmer96@gmail.com>
2026-02-01 20:04:12 +0900

    styles: 챗봇 스타일 테일윈드로

--- src/components/commons/GeminiChatbot.tsx
+++ src/components/commons/GeminiChatbot.tsx
@@ -9,7 +9,6 @@ import ReactMarkdown from "react-markdown";
 
 import BasicTooltip from "./BasicTooltip";
 
-import "./GeminiChatbot.css";
 import type { Content } from "@google/genai";
 
 interface IData {
@@ -100,9 +99,12 @@ export default function GeminiChatbot({ modelText, instruction }: { modelText: s
       <PopoverContent className="w-80 mb-1 mr-4 font-['Roboto']">
         <div ref={scrollRef} className="overflow-x-auto flex flex-col h-50 pr-4 mb-4 text-sm">
           {data?.history?.map((m, idx) => (
-            <div key={`${m.parts?.[0].text}_${idx}`} className={`my-4 py-2 space-y-4 ${m.role === "model" ? "self-start" : "px-4 self-end border rounded-xl"}`}>
+            <div
+              key={`${m.parts?.[0].text}_${idx}`}
+              className={`[&_ol]:list-decimal [&_ul]:list-disc [&_li]:ml-4
+               my-4 py-2 space-y-4 ${m.role === "model" ? "self-start" : "px-4 self-end border rounded-xl"}`}
+            >
               <ReactMarkdown>{m.parts?.[0].text}</ReactMarkdown>
-              {/* {m.parts?.[0].text} */}
             </div>
           ))}
 
